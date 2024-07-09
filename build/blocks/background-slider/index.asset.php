ba41dfef1e0ce91fbab0e3bca50c0b6e56ea0619
leemon <tacrapo@gmail.com>
2024-07-09 10:36:42 +0200

    Show a simple slider in block editor

--- build/blocks/background-slider/index.asset.php
+++ build/blocks/background-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '385446ba34336b4dca96');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e6e7203e312e390df6e0');
--- src/blocks/background-slider/edit.js
+++ src/blocks/background-slider/edit.js
@@ -15,9 +15,10 @@ import {
     MediaPlaceholder,
     useBlockProps,
 } from '@wordpress/block-editor';
-import { useEffect } from '@wordpress/element';
-import { useRefEffect } from '@wordpress/compose';
-import { useSelect } from '@wordpress/data';
+import {
+    useState,
+    useEffect,
+} from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -30,15 +31,15 @@ const Edit = (props) => {
         setAttributes,
     } = props;
 
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     const displayImages = (images) => {
         return (
             images.map((image, index) => {
                 return (
-                    <div className="carousel-item" key={index}>
-                        <figure>
-                            <img src={image.url} alt={image.alt} key={image.id} />
-                        </figure>
-                    </div>
+                    <figure className="wp-block-occ-rather-simple-background-slider__item" key={index}>
+                        <img src={image.url} alt={image.alt} key={image.id} className={`wp-block-occ-rather-simple-background-slider__slide ${index === currentIndex ? 'active' : ''}`} />
+                    </figure>
                 )
             })
         )
@@ -46,15 +47,23 @@ const Edit = (props) => {
 
     function setImages(value) {
         const imageDetails = value.map(image => (
-			{
-				id: image.id,
-				url: image.url,
-				alt: image.alt,
-				caption: image.caption
-			}
-		));
-		setAttributes({ images: imageDetails })
+            {
+                id: image.id,
+                url: image.url,
+                alt: image.alt,
+                caption: image.caption
+            }
+        ));
+        setAttributes({ images: imageDetails })
     }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 15000);
+
+        return () => clearInterval(interval);
+    }, [images.length]);
 
     return (
         <>
@@ -72,7 +81,7 @@ const Edit = (props) => {
                                 allowedTypes={['image']}
                                 multiple={true}
                                 gallery={true}
-                                value={images}
+                                value={images.map((image) => image.id)}
                                 onSelect={setImages}
                                 render={({ open }) => (
                                     <ToolbarButton onClick={open}>
@@ -86,7 +95,7 @@ const Edit = (props) => {
             <MediaUploadCheck>
                 {images.length > 0 ?
                     <figure {...blockProps}>
-                        <div className="background-slider-items">
+                        <div className="wp-block-occ-rather-simple-background-slider__items">
                             {
                                 displayImages(images)
                             }
