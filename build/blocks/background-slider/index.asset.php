6b80b6b0220a719009dd09f86de9c85de9433c07
leemon <tacrapo@gmail.com>
2024-07-10 00:25:03 +0200

    Fix typo

--- build/blocks/background-slider/index.asset.php
+++ build/blocks/background-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '54228ea13dfd47333c81');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '078414ab9f81f399fd32');
